1046709dbb7d50354481fa82ac15dda651b45abd
japheth198 <japhethjuur@gmail.com>
2021-05-25 12:30:35 +0300

    Proov

--- AutopoeApp/app/src/main/java/com/example/autopoeapp/CarsActivity.java
+++ AutopoeApp/app/src/main/java/com/example/autopoeapp/CarsActivity.java
@@ -1,10 +1,12 @@
 package com.example.autopoeapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CarsActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class CarsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
     }
+
     public void onOpen(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
         startActivity(intent);
@@ -28,6 +31,24 @@ public class CarsActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Order2Activity.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.account:
+                Intent account = new Intent(CarsActivity.this, AccountActivity.class);
+                startActivity(account);
+                break;
+            case R.id.logout:
+                Intent login = new Intent(CarsActivity.this, MainActivity.class);
+                login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(login);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
