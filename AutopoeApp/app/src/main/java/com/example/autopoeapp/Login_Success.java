2c3f2dff20e09fffeb1362e13edc3d090b09789f
japheth198 <japhethjuur@gmail.com>
2021-05-25 09:38:34 +0300

    rakenduse muutmine

--- AutopoeApp/app/src/main/java/com/example/autopoeapp/Login_Success.java
+++ AutopoeApp/app/src/main/java/com/example/autopoeapp/Login_Success.java
@@ -1,7 +1,9 @@
 package com.example.autopoeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Success extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Login_Success extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+    }
+    public void onForward(View view) {
+        Intent intent = new Intent(this, CarsActivity.class);
+        startActivity(intent);
     }
 }
--- AutopoeApp/app/src/main/java/com/example/autopoeapp/Login_Sucess.java
+++
@@ -1,6 +0,0 @@
-package com.example.autopoeapp;
-
-import android.app.Activity;
-
-public class Login_Sucess extends Activity {
-}
