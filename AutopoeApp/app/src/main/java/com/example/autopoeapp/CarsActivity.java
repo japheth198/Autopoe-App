7300c24d31d3372b11af7182d6a33dcaa60d7a66
Opilane TTHK <opilane@tthk.ee>
2021-05-25 10:37:08 +0300

    Lisansin menu_itemi carsActivitisse

--- AutopoeApp/app/src/main/java/com/example/autopoeapp/CarsActivity.java
+++ AutopoeApp/app/src/main/java/com/example/autopoeapp/CarsActivity.java
@@ -3,6 +3,8 @@ package com.example.autopoeapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class CarsActivity extends AppCompatActivity {
@@ -25,5 +27,12 @@ public class CarsActivity extends AppCompatActivity {
     public void onOpen2(View view) {
         Intent intent = new Intent(this, Order2Activity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+        return true;
     }
 }
